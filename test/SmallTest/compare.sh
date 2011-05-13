c07dbc204bdd9d46f67d74ccbb91586895e874e2
rob.hooft@nbic.nl <rob.hooft@nbic.nl@bf327465-11ba-4702-ae44-9666294d8988>
2011-05-13 11:20:07 +0000

    * Added test data  * Rewrote comparison with test results to accept running in a SVN directory  * Added actually running the test

---
+++ test/SmallTest/compare.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+for f in sim1chrVs20305_{BP,D,INV,LI,SI,TD}
+do
+	diff -c TargetOutput/$f ActualOutput/$f || (echo Not identical ; exit 1)
+done
+echo Results identical to expected output. OK.
+exit 0
