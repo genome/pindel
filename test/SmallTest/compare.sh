2a4f931f6cd820b2093fab6df51452a156b7f542
kees.burger@nbic.nl <kees.burger@nbic.nl@bf327465-11ba-4702-ae44-9666294d8988>
2011-05-16 07:59:36 +0000

    Added sanity checks on brace expansion. Needs additional work: exit 1 happens in subshell space scope.

--- test/SmallTest/compare.sh
+++ test/SmallTest/compare.sh
@@ -2,6 +2,6 @@
 for f in sim1chrVs20305_{BP,D,INV,LI,SI,TD}
 do
 	diff -c TargetOutput/$f ActualOutput/$f || (echo Not identical ; exit 1)
-done
+done || (echo Something went wrong ; exit 1)
 echo Results identical to expected output. OK.
 exit 0
