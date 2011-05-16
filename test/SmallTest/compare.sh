e974176d018a5717375f7530f844fceef8b7f443
david.vanenckevort@nbic.nl <david.vanenckevort@nbic.nl@bf327465-11ba-4702-ae44-9666294d8988>
2011-05-16 08:22:56 +0000

    Handle error conditions in compare.sh better

--- test/SmallTest/compare.sh
+++ test/SmallTest/compare.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 for f in sim1chrVs20305_{BP,D,INV,LI,SI,TD}
 do
-	diff -c TargetOutput/$f ActualOutput/$f || (echo Not identical ; exit 1)
-done || (echo Something went wrong ; exit 1)
+	if ! diff -c TargetOutput/$f ActualOutput/$f
+        then
+            echo 'Not identical'
+            exit 1
+        fi
+done
+if test $? -ne 0
+then
+  echo 'For loop failed'
+  exit 1
+fi
 echo Results identical to expected output. OK.
 exit 0
