24165c69adb63d71044b12e6e61ecb8db3d7cfcf
eric_wubbo@hotmail.com <eric_wubbo@hotmail.com@bf327465-11ba-4702-ae44-9666294d8988>
2012-08-13 14:58:27 +0000

    gave pindel.cpp the new version number (0.2.4t)

--- src/pindel.cpp
+++ src/pindel.cpp
@@ -68,8 +68,9 @@
 /* Kai: update 0.2.4q: use ChrName and ChrSeq for clarity; start to include assembly */
 /* Kai: min_perfect_match_around_BP to 5 */
 /* EW: update 0.2.4s: bugfix for -p option of Pindel0.2.4r */
+/* EW: update 0.2.4t, updates now shown in RELEASE document in trunk directory */
 
-const std::string Pindel_Version_str = "Pindel version 0.2.4s, June 18 2012.";
+const std::string Pindel_Version_str = "Pindel version 0.2.4t, August 13 2012.";
 
 std::ofstream g_logFile;
 
