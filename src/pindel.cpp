b47fc3e7f8e930fa2780263b02ac166d10a2c56c
liangkaiye@gmail.com <liangkaiye@gmail.com>
2014-05-02 10:11:57 -0500

    version number changed and test git

--- src/pindel.cpp
+++ src/pindel.cpp
@@ -74,7 +74,7 @@
 /* EW: update 0.2.4s: bugfix for -p option of Pindel0.2.4r */
 /* EW: update 0.2.4t, updates now shown in RELEASE document in trunk directory */
 
-const std::string Pindel_Version_str = "Pindel version 0.2.5a3, Oct 24 2013.";
+const std::string Pindel_Version_str = "Pindel version 0.2.5a4, May 2 2014.";
 
 const Chromosome g_dummyChromosome("","");
 Genome g_genome;
