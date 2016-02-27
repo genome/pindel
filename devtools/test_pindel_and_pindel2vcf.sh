3281f648299a4c2da6f3cdf0479e15746419fc4c
EWLameijer <eric_wubbo@hotmail.com>
2016-02-27 12:17:24 +0100

    DevTools now with small test suite (pindel only for now) - pindel2vcf yet to come - hopefully¨ : :

---
+++ devtools/test_pindel_and_pindel2vcf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+../pindel -f ../test/SmallTest/hs_ref_chr20.fa -p ../test/SmallTest/COLO-829_20-p_ok-1.txt -o test_results/colo_test.out -k -s -l -T 4
+
+../pindel -f ../test/SmallTest/sim1chrVs2.fa -i ../test/SmallTest/sim1chrVs2.conf_for_test -o test_results/simulated_test.out -k -s -l -T 4
+
+
+diff -q gold_standard test_results
+
