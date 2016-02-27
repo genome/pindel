2299f35466dce5c925097604c2a5af79945a8c2c
EWLameijer <eric_wubbo@hotmail.com>
2016-02-27 14:57:12 +0100

    Updating GS to single-threaded values

---
+++ devtools/run_regressiontests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+../pindel -f ../test/SmallTest/hs_ref_chr20.fa -p ../test/SmallTest/COLO-829_20-p_ok-1.txt -o test_results/colo_test.out -k -s -l -T 1
+
+../pindel -f ../test/SmallTest/sim1chrVs2.fa -i ../test/SmallTest/sim1chrVs2.conf_for_test -o test_results/simulated_test.out -k -s -l -T 1
+
+echo
+echo "Start of diffs"
+diff -q gold_standard test_results
+echo "End of diffs"
+
