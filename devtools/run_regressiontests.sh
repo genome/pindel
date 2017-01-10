05fa9b72b27c20b64ecff6ebd38c0a14c9bc8be1
EWLameijer <e_lameijer@mail.xjtu.edu.cn>
2017-01-10 09:49:27 +0800

    updated the regression test to include pindel2vcf, and removed some dead code

--- devtools/run_regressiontests.sh
+++ devtools/run_regressiontests.sh
@@ -6,6 +6,8 @@ mkdir test_results
 
 ../pindel -f ../test/SmallTest/sim1chrVs2.fa -i ../test/SmallTest/sim1chrVs2.conf_for_test -o test_results/simulated_test.out -k -s -l -T 1
 
+../pindel2vcf -R SIMCHROM -r ../test/SmallTest/sim1chrVs2.fa -P test_results/simulated_test.out -d 00000000
+
 echo
 echo "Start of diffs"
 diff -q gold_standard test_results
